5f3607d5436918d7395d8895e9f08fb401cdae66
xiaodong-java <946518389@qq.com>
2016-11-10 15:02:31 +0800

    init

---
+++ spring-cloud-config/src/main/java/com/cloud/soft/ApplicationConfigMain.java
@@ -0,0 +1,17 @@
+package com.cloud.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+public class ApplicationConfigMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConfigMain.class, args);
+	}
+
+}
